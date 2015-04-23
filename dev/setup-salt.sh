09073ebed81c60dde8376d649668672cc4d3270a
Julien Lavergne <julien@x-wing.local>
2015-04-23 11:08:39 +0200

    optimized setup bootstrap

--- dev/setup-salt.sh
+++ dev/setup-salt.sh
@@ -2,7 +2,6 @@
 sudo apt-get update -y
 sudo apt-get install python-software-properties pkg-config software-properties-common -y
 sudo add-apt-repository ppa:saltstack/salt -y
-sudo apt-get update -y
 sudo apt-get install salt-master -y
 sudo apt-get install salt-minion -y
 # setup top files to test the formula
