fc1d0b15a3d5bce9948f37a1d015692eddf9c1b3
Imran Iqbal <iqbalmy@hotmail.com>
2019-09-17 22:56:38 +0100

    refactor(kitchen+travis+inspec): use pre-salted images
    * Semi-automated using https://github.com/myii/ssf-formula/pull/36
    * Restructure all tests according to updated test suites
    * Improve tests where necessary (code style, new platforms and package versions)

--- test/integration/v201803-py2/controls/pkgs_spec.rb
+++ test/integration/v201803-py2/controls/pkgs_spec.rb
@@ -1,3 +1,11 @@
+version =
+  case platform[:family]
+  when 'redhat'
+    '2018.3.4-1.el7'
+  when 'debian'
+    '2018.3.4+ds-1'
+  end
+
 control 'salt packages' do
   title 'should be installed'
 
@@ -7,6 +15,7 @@ control 'salt packages' do
   ).each do |p|
     describe package(p) do
       it { should be_installed }
+      its('version') { should eq version }
     end
   end
 end
--- test/salt/pillar/develop-py3.sls
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# vim: ft=yaml
----
-salt:
-  release: 'latest'
-  py_ver: 'py3'
