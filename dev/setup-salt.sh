5386e08ba429317c6f685e6789e0cadd8ec472f3
Niels Abspoel <aboe@spectre.wraith.lan>
2019-11-28 19:42:14 +0100

    fix(shellcheck): fix shellcheck error

--- dev/setup-salt.sh
+++ dev/setup-salt.sh
@@ -2,7 +2,7 @@
 
 # use the latest stable Salt from repo.saltstack.com
 wget -O - https://repo.saltstack.com/apt/ubuntu/14.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
-sudo echo 'deb http://repo.saltstack.com/apt/ubuntu/14.04/amd64/latest trusty main' > /etc/apt/sources.list.d/saltstack.list
+echo 'deb http://repo.saltstack.com/apt/ubuntu/14.04/amd64/latest trusty main' | sudo tee /etc/apt/sources.list.d/saltstack.list
 
 sudo apt-get update -y
 sudo apt-get install salt-master -y
