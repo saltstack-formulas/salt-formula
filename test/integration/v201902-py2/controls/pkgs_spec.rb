02af6420a32a00f03c59a350cc926db68434b485
N <noel.mcloughlin@gmail.com>
2020-01-22 12:04:32 +0000

    test(amazonlinux): match package version against os

--- test/integration/v201902-py2/controls/pkgs_spec.rb
+++ test/integration/v201902-py2/controls/pkgs_spec.rb
@@ -1,9 +1,14 @@
 version =
   case platform[:family]
   when 'redhat'
-    '2019.2.1-1.el7'
-  when 'fedora'
-    '2019.2.0-1.fc30'
+    case platform[:name]
+    when 'amazon'
+      '2019.2.3-1.el7'
+    when 'centos'
+      '2019.2.1-1.el7'
+    when 'fedora'
+      '2019.2.0-1.fc30'
+    end
   when 'suse'
     '2019.2.0-lp151.5.3.1'
   when 'debian'
