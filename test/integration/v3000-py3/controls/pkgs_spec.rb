f91f10c2800edfc1b59fd731a6b6f82a47f74fcc
Imran Iqbal <iqbalmy@hotmail.com>
2020-03-20 23:46:45 +0000

    test(neon): add pillar and tests for `3000`

--- test/integration/v3000-py3/controls/pkgs_spec.rb
+++ test/integration/v3000-py3/controls/pkgs_spec.rb
@@ -5,16 +5,16 @@ version =
   when 'redhat'
     case platform[:name]
     when 'amazon'
-      '2019.2.3-1.amzn2'
+      '3000-1.amzn2'
     when 'centos'
-      '2019.2.3-1.el8'
+      '3000-1.el8'
     end
   when 'fedora'
-    '2019.2.1rc0-3.fc31'
+    '3000-5.fc31'
   when 'suse'
-    '2019.2.0-lp151.28.1'
+    '3000-lp151.28.1'
   when 'debian'
-    '2019.2.3+ds-1'
+    '3000+ds-1'
   end
 
 control 'salt packages' do
---
+++ test/salt/pillar/v3000-py3.sls
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# vim: ft=yaml
+---
+salt:
+  release: '3000'
+  py_ver: 'py3'
