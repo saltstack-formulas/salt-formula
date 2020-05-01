c228029879f36ee5aaa5f3cd3d3684ede29808bb
Imran Iqbal <iqbalmy@hotmail.com>
2020-05-01 08:32:39 +0100

    test: add tests for `v3000-py2`

---
+++ test/integration/v3000-py2/controls/pkgs_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+version =
+  case platform[:family]
+  when 'debian'
+    '3000.2+ds-1'
+  end
+
+control 'salt packages' do
+  title 'should be installed'
+
+  %w[
+    salt-master
+    salt-minion
+  ].each do |p|
+    describe package(p) do
+      it { should be_installed }
+      its('version') { should eq version }
+    end
+  end
+end
---
+++ test/salt/pillar/v3000-py2.sls
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# vim: ft=yaml
+---
+salt:
+  release: '3000'
