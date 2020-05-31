b2b1fee370060c82101526157dc2630a4453dfb8
Imran Iqbal <iqbalmy@hotmail.com>
2020-05-31 22:59:26 +0100

    test(pkgs_spec): simplify using `match` (trigger: update for `suse`)
    * https://travis-ci.org/github/myii/salt-formula/jobs/692337807#L2366-L2381
      - `opensuse` now has `3000.3` available
    * Simplification is useful anyway, even without this (could still use `case`)

--- test/integration/v201902-py2/controls/pkgs_spec.rb
+++ test/integration/v201902-py2/controls/pkgs_spec.rb
@@ -1,27 +1,9 @@
 # frozen_string_literal: true
 
-# Prepare platform "finger"
-platform_finger = "#{platform[:name]}-#{platform[:release].split('.')[0]}"
-
-version =
-  case platform[:family]
-  when 'debian'
-    '2019.2.5+ds-1'
-  when 'redhat'
-    case platform_finger
-    when 'centos-7'
-      '2019.2.5-1.el7'
-    when 'centos-6'
-      '2019.2.5-1.el6'
-    when 'amazon-2'
-      '2019.2.5-1.amzn2'
-    when 'amazon-2018'
-      '2019.2.5-1.amzn1'
-    end
-  end
-
 control 'salt packages' do
   title 'should be installed'
+
+  version = '2019.2.5'
 
   %w[
     salt-master
@@ -29,7 +11,7 @@ control 'salt packages' do
   ].each do |p|
     describe package(p) do
       it { should be_installed }
-      its('version') { should eq version }
+      its('version') { should match(/^#{version}/) }
     end
   end
 end
--- test/integration/v3000-py3/controls/pkgs_spec.rb
+++ test/integration/v3000-py3/controls/pkgs_spec.rb
@@ -1,35 +1,9 @@
 # frozen_string_literal: true
 
-# Prepare platform "finger"
-platform_finger = "#{platform[:name]}-#{platform[:release].split('.')[0]}"
-
-version =
-  case platform[:family]
-  when 'debian'
-    '3000.3+ds-1'
-  when 'redhat'
-    case platform_finger
-    when 'centos-8'
-      '3000.3-1.el8'
-    when 'centos-7'
-      '3000.3-1.el7'
-    when 'amazon-2'
-      '3000.3-1.amzn2'
-    end
-  when 'fedora'
-    '3000.3-1.fc31'
-  when 'suse'
-    # Issue in the upstream repo, should be `3000.3`
-    case platform[:release]
-    when /^15.2/
-      '3000.2-lp152.7.1'
-    when /^15.1/
-      '3000.2-lp151.2.1'
-    end
-  end
-
 control 'salt packages' do
   title 'should be installed'
+
+  version = '3000.3'
 
   %w[
     salt-master
@@ -37,7 +11,7 @@ control 'salt packages' do
   ].each do |p|
     describe package(p) do
       it { should be_installed }
-      its('version') { should eq version }
+      its('version') { should match(/^#{version}/) }
     end
   end
 end
