d4460718c5710fe1b38ed1a690a8725c8a4185f5
Dafydd Jones <dafydd@techneg.it>
2020-10-07 09:09:56 +0100

    chore(commitlint): add `{body,footer,header}-max(-line)-length` [skip ci]
    * Automated using https://github.com/myii/ssf-formula/pull/265

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,8 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
+    rules: {
+        'body-max-line-length': [2, 'always', 120],
+        'footer-max-line-length': [2, 'always', 120],
+        'header-max-length': [2, 'always', 72],
+    },
 };
